e6e88fa105408ee9e6f6c1755f8e642707545f56
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2021-10-19 19:19:13 -0500

    not working

--- fourier.py
+++ fourier.py
@@ -2,25 +2,24 @@ import math
 import cmath
 import matplotlib.pyplot as plt
 
-def X(k, T, T1, w0):
+def X(k, T, T1):
     if(k == 0):
         return 2*T1/T
-    return 2 * math.sin(k * w0 * T1) / (k * w0 * T)
+    return 2 * math.sin(k * 2*math.pi/T * T1) / (k * 2*math.pi/T * T)
 
-def x(t, w0):
+def x(t, T, T1):
     sum = 0j
-    k = -100000
-    while k <= 100000:
-        sum = X(k, 4, 1, w0) * cmath.exp(1j * k  * w0 * t)
+    k = -1000
+    while k <= 1000:
+        sum = X(k, T, T1) * cmath.exp(1j * k  * 2*math.pi/T * t)
         k += 1
     return sum
 
-
 xVals = []
 yVals = []
-i = -10
-while i <= 10:
-    xVals.append(i)
-    yVals.append(x(i, 2*math.pi/4))
-    print(i, yVals[i + 10])
-    i += 1
+t = -10
+while t <= 10:
+    xVals.append(t)
+    yVals.append(x(t, 10, 2))
+    print(t, "\t", yVals[t + 10])
+    t += 1
